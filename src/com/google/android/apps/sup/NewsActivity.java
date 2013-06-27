ad7198821485ef71c4c30c4aa80fc604665af970
Rotem Mayo <rotemm11@cs.huji.ac.il>
2013-06-27 10:20:42 +0300

    hello

--- src/com/google/android/apps/sup/NewsActivity.java
+++ src/com/google/android/apps/sup/NewsActivity.java
@@ -13,6 +13,8 @@ import messeges.*;
 
 public class NewsActivity extends Activity {
 
+	private ArrayList<Messege> messeges;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -20,6 +22,7 @@ public class NewsActivity extends Activity {
 		getFeed(GlobalInfo.session);
 	}
 
+	//reutrns the final news feed
 	public void getFeed(Session session) {
 		Bundle params = new Bundle();
 		params.putString("limit", "0");
@@ -27,8 +30,9 @@ public class NewsActivity extends Activity {
 		Request request = new Request(session, "me/home", params,
 				HttpMethod.GET, new Request.Callback() {
 					public void onCompleted(Response response) {
-						ArrayList<JSONObject> vals = buildValsFromResponse(response);
-						sort(vals);
+						ArrayList<JSONObject> JSONmessages = buildValsFromResponse(response);
+						sort(JSONmessages);
+						messeges = parse(JSONmessages);
 
 					}
 
@@ -36,6 +40,7 @@ public class NewsActivity extends Activity {
 		request.executeAsync();
 	}
 
+	//reutrns an array of JSON objects, each elemnt is a message
 	public ArrayList<JSONObject> buildValsFromResponse(Response response) {
 
 		JSONObject jo = response.getGraphObject().getInnerJSONObject();
@@ -58,6 +63,7 @@ public class NewsActivity extends Activity {
 		return arr;
 	}
 
+	//dells all irrelevent messages
 	public ArrayList<JSONObject> sort(ArrayList<JSONObject> arr) {
 
 		for (int i = 0; i < arr.size(); i++) {
@@ -67,6 +73,7 @@ public class NewsActivity extends Activity {
 		return arr;
 	}
 
+	//parses the final news feed array from JSON Objects into messages
 	public ArrayList<Messege> parse(ArrayList<JSONObject> arr){
 		
 		ArrayList<Messege> mes = new ArrayList<Messege>();
@@ -80,28 +87,31 @@ public class NewsActivity extends Activity {
 				
 				//checking for pic
 					if(jo.has("picture")){
-				
-						mes.add(new PicMessege(jo.getString("name"), jo.getString("message"), jo.getString("created_time"), null));
-				
+						mes.add(new PicMessege(jo.getString("name"), jo.getString("message"), jo.getString("created_time"), jo.getString("picture")));
 					}
 				
 			
 					//checking for link
 					else if(jo.has("link")){
-				
+						mes.add(new LinkMessege(jo.getString("name"), jo.getString("message"), jo.getString("created_time"), jo.getString("link")));
 					}
 			
 					//default
 					else{
-						
+						mes.add(new Messege(jo.getString("name"), jo.getString("message"), jo.getString("created_time")));
 					}
 					
 			} catch (JSONException e) {
-				// TODO Auto-generated catch block
+				System.out.println("the JSON message dosen't exist! :(");
 				e.printStackTrace();
 			}
 		}
+		
 		return null;
+	}
+	
+	public ArrayList<Messege> getMesseges(){
+		return messeges;
 	}
 
 	@Override
