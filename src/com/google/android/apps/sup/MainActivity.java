90f053c766f45b4a2bb03d0d048395d94415c4ff
Asaf Vaknin <asaf11@cs.huji.ac.il>
2013-06-27 10:30:26 +0300

    leavin it for mervin

--- src/com/google/android/apps/sup/MainActivity.java
+++ src/com/google/android/apps/sup/MainActivity.java
@@ -6,10 +6,10 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 
-public class MainActivity extends FragmentActivity implements OnClickListener{
-
+public class MainActivity extends FragmentActivity {
 	private MainFragment mainFragment;
 	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
@@ -28,17 +28,9 @@ public class MainActivity extends FragmentActivity implements OnClickListener{
 	    }
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
