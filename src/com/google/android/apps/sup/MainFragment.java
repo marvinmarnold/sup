6f3534b38af9f574b6a38311a16464bde728ea1b
Mohannad Jamal <mohannadj11@cs.huji.ac.il>
2013-06-27 18:50:16 +0300

    Working version

--- src/com/google/android/apps/sup/MainFragment.java
+++ src/com/google/android/apps/sup/MainFragment.java
@@ -34,11 +34,11 @@ public class MainFragment extends Fragment {
 	private void onSessionStateChange(Session session, SessionState state, Exception exception) {
 		System.out.println("logged in!!! yay!!!");
 	    if (state.isOpened()) {
-//	    	Intent intent = new Intent();
-//	    	intent.setClass(getActivity(), NewsActivity.class);
-//	    	GlobalInfo.session = session;
+	    	Intent intent = new Intent();
+	    	intent.setClass(getActivity(), NewsActivity.class);
+	    	GlobalInfo.session = session;
 	    	Log.i(TAG, "Logged in...");
-//	    	startActivity(intent);
+	    	startActivity(intent);
 	    } else if (state.isClosed()) {
 	        Log.i(TAG, "Logged out...");
 	    }
