bfa593846a7c1249829da58e00e5558a83685a32
Mohannad Jamal <jamalmohannad@gmail.com>
2013-07-01 13:03:30 +0300

    Filter working

--- src/com/google/android/apps/sup/NewsActivity.java
+++ src/com/google/android/apps/sup/NewsActivity.java
@@ -12,37 +12,47 @@ import org.json.JSONObject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.AdapterView.OnItemClickListener;
 
 import com.facebook.HttpMethod;
 import com.facebook.Request;
 import com.facebook.Response;
 import com.facebook.Session;
 
-
 public class NewsActivity extends Activity {
+	ListView messageListView;
+	
+	// Listview Adapter
+	ArrayAdapter<String> arrayAdapter;
+     
+    // Search EditText
+    EditText inputSearch;
 	
 	ArrayList<String> MessageList;
 	
 	private ArrayList<Messege> messeges;
 	private static final String TAG = "NewsActivity";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_news);
-		if(GlobalInfo.session.isOpened()){
+		if (GlobalInfo.session.isOpened()) {
 			getFeed(GlobalInfo.session);
 		}
 		Log.v(TAG, "Got to onCreate()");
 	}
 
-	//reutrns the final news feed
+	
+	// reutrns the final news feed
 	public void getFeed(Session session) {
 		Bundle params = new Bundle();
 		params.putString("limit", "0");
@@ -54,60 +64,62 @@ public class NewsActivity extends Activity {
 						sort(JSONmessages);
 						try {
 							messeges = parse(JSONmessages);
-							ListView messageListView = (ListView) findViewById(R.id.listViewMessages);
+							messageListView = (ListView) findViewById(R.id.listViewMessages);
+							inputSearch = (EditText) findViewById(R.id.inputSearch);
 							MessageList = new ArrayList<String>();
-							for(int i=0; i<messeges.size(); i++){
+							for (int i = 0; i < messeges.size(); i++) {
 								MessageList.add(messeges.get(i).getText());
 							}
-							
-					// Create The Adapter with passing ArrayList as 3rd parameter
-					ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(NewsActivity.this,
-								android.R.layout.simple_list_item_1, MessageList);
-			//		 Set The Adapter
-						messageListView.setAdapter(arrayAdapter);
-						
+
+							// Create The Adapter with passing ArrayList as 3rd
+							// parameter
+							arrayAdapter = new ArrayAdapter<String>(
+									NewsActivity.this,
+									android.R.layout.simple_list_item_1,
+									MessageList);
+							// Set The Adapter
+							messageListView.setAdapter(arrayAdapter);
+							inputSearch.addTextChangedListener(new TextWatcher() {
+							     
+							    @Override
+							    public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
+							        // When user changed the Text
+							        NewsActivity.this.arrayAdapter.getFilter().filter(cs);  
+							    }
+							     
+							    @Override
+							    public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+							            int arg3) {
+							        // TODO Auto-generated method stub
+							         
+							    }
+							     
+							    @Override
+							    public void afterTextChanged(Editable arg0) {
+							        // TODO Auto-generated method stub                         
+							    }
+							});	
 						} catch (JSONException e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
-						
-						
+
 					}
 
 				});
 		request.executeAsync();
 	}
-	// argument position gives the index of item which is clicked
-				public void onItemClick(AdapterView<?> arg0, View v, int position,
-						long arg3) {
-
-				}
-
-
-		
-
-		void getAnimalNames() {
-			MessageList.add("DOG");
-			MessageList.add("CAT");
-			MessageList.add("HORSE");
-			MessageList.add("ELEPHANT");
-			MessageList.add("LION");
-			MessageList.add("COW");
-			MessageList.add("MONKEY");
-			MessageList.add("DEER");
-			MessageList.add("RABBIT");
-			MessageList.add("BEER");
-			MessageList.add("DONKEY");
-			MessageList.add("LAMB");
-			MessageList.add("GOAT");
-
-		}
 	
-	public void sanityCheck(JSONObject object){
+	// argument position gives the index of item which is clicked
+	public void onItemClick(AdapterView<?> arg0, View v, int position, long arg3) {
+
+	}
+
+	public void sanityCheck(JSONObject object) {
 		Log.i(TAG, object.names().toString());
 	}
-	
-	//reutrns an array of JSON objects, each elemnt is a message
+
+	// reutrns an array of JSON objects, each elemnt is a message
 	public ArrayList<JSONObject> buildValsFromResponse(Response response) {
 
 		JSONObject jo = response.getGraphObject().getInnerJSONObject();
@@ -123,14 +135,14 @@ public class NewsActivity extends Activity {
 
 			}
 		} catch (JSONException e) {
-	    	Log.i(TAG, "no response");
+			Log.i(TAG, "no response");
 			e.printStackTrace();
 		}
 
 		return arr;
 	}
 
-	//dells all irrelevent messages
+	// dells all irrelevent messages
 	public ArrayList<JSONObject> sort(ArrayList<JSONObject> arr) {
 
 		for (int i = 0; i < arr.size(); i++) {
@@ -140,63 +152,70 @@ public class NewsActivity extends Activity {
 		return arr;
 	}
 
-	//parses the final news feed array from JSON Objects into messages
-	public ArrayList<Messege> parse(ArrayList<JSONObject> arr) throws JSONException{
-		
+	// parses the final news feed array from JSON Objects into messages
+	public ArrayList<Messege> parse(ArrayList<JSONObject> arr)
+			throws JSONException {
+
 		ArrayList<Messege> mes = new ArrayList<Messege>();
 		JSONObject jo;
-		
-		for(int i=0; i<arr.size(); i++){
-			
+
+		for (int i = 0; i < arr.size(); i++) {
+
 			jo = arr.get(i);
 			sanityCheck(jo);
 			try {
-				
-				//checking for pic
-					if(jo.has("picture")){
-						mes.add(new PicMessege(jo.getJSONObject("from").getString("name"), getTextByJsonobject(jo), jo.getString("created_time"), jo.getString("picture")));
-					}
-				
-			
-					//checking for link
-					else if(jo.has("link")){
-						mes.add(new LinkMessege(jo.getJSONObject("from").getString("name"), getTextByJsonobject(jo), jo.getString("created_time"), jo.getString("link")));
-					}
-			
-					//default
-					else{
-						mes.add(new Messege(jo.getJSONObject("from").getString("name"), jo.getString("message"), jo.getString("created_time")));
-					}
-					
+
+				// checking for pic
+				if (jo.has("picture")) {
+					mes.add(new PicMessege(jo.getJSONObject("from").getString(
+							"name"), getTextByJsonobject(jo), jo
+							.getString("created_time"), jo.getString("picture")));
+				}
+
+				// checking for link
+				else if (jo.has("link")) {
+					mes.add(new LinkMessege(jo.getJSONObject("from").getString(
+							"name"), getTextByJsonobject(jo), jo
+							.getString("created_time"), jo.getString("link")));
+				}
+
+				// default
+				else {
+					mes.add(new Messege(jo.getJSONObject("from").getString(
+							"name"), jo.getString("message"), jo
+							.getString("created_time")));
+				}
+
 			} catch (JSONException e) {
-				Log.i(TAG,"the JSON message dosen't exist! :(");
+				Log.i(TAG, "the JSON message dosen't exist! :(");
 				e.printStackTrace();
 			}
 		}
-		
+
 		return mes;
 	}
-	
-	public ArrayList<Messege> getMesseges(){
+
+	public ArrayList<Messege> getMesseges() {
 		return messeges;
 	}
-	
+
 	public String getTextByJsonobject(JSONObject object) {
-		try{
+		try {
 			return object.getString("message");
-		}catch (JSONException e){
-			try{
+		} catch (JSONException e) {
+			try {
 				return object.getString("story");
-			} catch (JSONException E){
+			} catch (JSONException E) {
 				return "s";
 			}
 		}
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+		
 }
